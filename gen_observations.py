48addb6443fc91828863daf1f66882e1bcc0bcde
Fred Aleixo <91429411+fpaleixo@users.noreply.github.com>
2022-03-22 15:37:20 +0000

    Experimenting with observation grounding

--- gen_observations.py
+++ gen_observations.py
@@ -1,14 +1,28 @@
 import os, glob, clingo
+from clingo.application import ApplicationOptions
 
 #-----Configs-----
 
+observation_number = "2"
+time_steps = "3"
+model_path = "./lp_models/6.lp"
 ss_path = "./encodings/gen_observations.lp"
+
+
+
+#-----Auxiliary Functions-----
 
 def on_model(m):
     print (m)
 
-ctl = clingo.Control()
 
+
+#-----Main-----
+
+ctl = clingo.Control(arguments=["-c e=" + observation_number, "-c t=" + time_steps])
+
+ctl.load(model_path)
 ctl.load(ss_path)
 ctl.ground([("base", [])])
 ctl.solve(on_model=on_model)
+
