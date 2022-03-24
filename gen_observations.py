aa05cda183f49b2b148600177e874046744e2885
Fred Aleixo <fp.aleixo@campus.fct.unl.pt>
2022-03-24 19:51:44 +0000

    Generated observations are now saved to file

--- gen_observations.py
+++ gen_observations.py
@@ -1,12 +1,16 @@
-import os, glob, clingo
+import os, glob, clingo, re
 from clingo.application import ApplicationOptions
+
+#TODO - test correctness, do async encoding, support cmd line paths
 
 #-----Configs-----
 
 observation_number = "2"
 time_steps = "3"
-model_path = "./lp_models/6.lp"
+model_path = "./lp_models/1.lp"
 ss_path = "./encodings/gen_observations.lp"
+
+save_sync = "./lp_models/obsv/tseries/sync"
 
 
 
@@ -15,6 +19,27 @@ ss_path = "./encodings/gen_observations.lp"
 def on_model(m):
     print (m)
 
+def saveObsToFile(atoms):
+    experiments_observations = {}
+
+    current_path = os.path.join(save_sync, os.path.basename(model_path).replace(".lp", "-obs.lp"))
+    f = open(current_path, 'w')
+    
+    for atom in atoms:
+        if "experiment" in atom:
+            exp_num = ''.join(d for d in atom if d.isdigit())
+            experiments_observations[exp_num] = [atom]
+        
+        elif "observation" in atom:
+            terms = re.search('\((.*)\)', atom).group(1)
+            terms = terms.split(',')
+            experiments_observations[terms[0]] += [atom]
+
+    for item in experiments_observations.items():
+        sorted_atoms = sorted(item[1])
+        for atom in sorted_atoms:
+            f.write(atom+".\n")
+        f.write("\n")
 
 
 #-----Main-----
@@ -24,5 +49,10 @@ ctl = clingo.Control(arguments=["-c e=" + observation_number, "-c t=" + time_ste
 ctl.load(model_path)
 ctl.load(ss_path)
 ctl.ground([("base", [])])
-ctl.solve(on_model=on_model)
 
+atoms = []
+with ctl.solve(yield_=True) as handle:
+        for model in handle:
+            atoms += (str(model).split(" "))
+
+saveObsToFile(atoms)
