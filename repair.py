b9deaa46f420c0d40155afd3ed98e8b5464a1448
Fred Aleixo <fp.aleixo@campus.fct.unl.pt>
2022-04-20 11:33:15 +0100

    Worked on repairs
    - Made output of repair.py more readable
    - Added generation of all terms from ancestors.lp to all_candidates_stable.lp

--- repair.py
+++ repair.py
@@ -4,10 +4,17 @@ from aux_scripts.common import uniquify
 #-----Configs-----
 
 #Model path
-model_path = "./lp_models/test.lp"
+model_path = "lp_models/corrupted/3/3-corrupted-f.lp"
 
-#Paths of encondings for repairs
-repairs_path = "./encodings/repairs/all_candidates.lp"
+#Paths of expected observations
+obsv_path = "lp_models/obsv/sstate/3-obs.lp"
+
+#Paths of encodings with inconsistencies
+incst_path = "./lp_models/corrupted/3/inconsistencies/3-corrupted-f-stable_inconsistency.lp"
+
+#Paths of encodings for repairs
+repairs_path = "./encodings/repairs/all_candidates_stable.lp"
+
 
 #Mode flags 
 toggle_stable_state = True
@@ -22,16 +29,56 @@ global_logger.setLevel(logging.DEBUG)
 
 
 #-----Auxiliary Functions-----
-
-
-def printAncestors(atoms):
+def printRepairs(atoms):
   if not atoms:
     print("No answers sets could be found	\u2755 there must be something wrong with the encoding...")
   elif atoms[0]:
-    print("Ancestors found \u2714\uFE0F  They are:")
+    repairs = []
+    func_map = {}
     print(atoms)
+    for a in atoms:
+      if "generated_term" in a:
+        arguments = a.split(')')[0].split('(')[1].split(',')
+        func = arguments[0]
+        level = arguments[1]
+        clause = arguments[3]
+        variable = arguments[2]
+        
+        if func not in func_map.keys():
+          func_map[func] = {}
+
+        level_map = func_map[func]
+        
+        if level not in level_map.keys():
+          level_map[level] = {}
+
+        clause_map = level_map[level]
+
+        if clause not in clause_map.keys():
+          clause_map[clause] = [variable]
+        else:
+          clause_map[clause].append(variable)
+      
+      else:
+        repairs += a
+    
+    if func_map:
+        print("Generated terms found \u2714\uFE0F  They are:")
+        for func in func_map.keys():
+          print("--Printing possible terms for function of " + func +"--")
+          level_map = func_map[func]
+          for level in level_map.keys():
+            print("Level " + level)
+            clause_map = level_map[level]
+            for clause in clause_map.keys():
+              print(clause + ":" + str(clause_map[clause]),  end=" | ")
+            print()
+
+    if repairs:
+      print("Repairs found \u2714\uFE0F  They are:")
+      print(str(repairs))
   else: 
-    print("No ancestors could be found \u274C")
+    print("No repairs could be found \u274C")
 
 
 
@@ -39,6 +86,8 @@ def printAncestors(atoms):
 ctl = clingo.Control(arguments=[])
 
 ctl.load(model_path)
+ctl.load(obsv_path)
+ctl.load(incst_path)
 ctl.load(repairs_path)
 
 ctl.ground([("base", [])])
@@ -47,4 +96,4 @@ with ctl.solve(yield_=True) as handle:
   for model in handle:
     atoms = (str(model).split(" "))
 
-printAncestors(atoms)
+printRepairs(atoms)
