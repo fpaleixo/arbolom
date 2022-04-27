fde0b0d36ab509fd041a31dc895728e0d4e37b09
Fred Aleixo <fp.aleixo@campus.fct.unl.pt>
2022-04-27 19:20:14 +0100

    Started work on more efficient encodings
    -Created new encoding for term generation (now done more naively but with much more efficiency: for 10 variables takes less than 0.1s, as opposed to >70s with the old all_candidates_stable encoding). Output is then processed by python
    
    -Old repair script can be found in repair_old.py

--- repair.py
+++ repair.py
@@ -6,6 +6,8 @@ from aux_scripts.common import uniquify
 #Usage: $python repair.py
 #Note: Model, observations and inconsistencies to be used by the algorithm have to be specified in the configs below
 
+#TODO - comment things
+
 #-----Configs-----
 
 #Toggle debug mode
@@ -20,8 +22,11 @@ obsv_path = "lp_models/obsv/sstate/3-obs.lp"
 #Paths of encodings with inconsistencies
 incst_path = "./lp_models/corrupted/3/inconsistencies/3-corrupted-f-stable_inconsistency.lp"
 
-#Paths of encodings for repairs
-repairs_path = "./encodings/repairs/all_candidates_stable.lp"
+#Paths of encodings for generating terms
+termgen_path = "./encodings/repairs/term_generator.lp"
+
+#Paths of encodings for generating functions
+funcgen_path = "./encodings/repairs/func_generator.lp"
 
 #Mode flags 
 toggle_stable_state = True
@@ -43,124 +48,62 @@ def printStatistics(stats_dict):
   print("Total: "+str(times["total"]) + "s (Solving: "+str(times["solve"])+"s)")
   print("CPU Time: "+str(times["cpu"])+"s")
 
-#Inputs: term_map is the map created in displaySolution that will store term atoms.
-#Purpose: Builds the term map by storing each term atom.
-def buildTermMap(term_map, atom):
-  arguments = atom.split(')')[0].split('(')[1].split(',')
-  func = arguments[0]
-  level = arguments[1]
-  clause = arguments[3]
-  variable = arguments[2]
-  
-  if func not in term_map.keys():
-    term_map[func] = {}
-
-  level_map = term_map[func]
-  
-  if level not in level_map.keys():
-    level_map[level] = {}
-
-  clause_map = level_map[level]
-
-  if clause not in clause_map.keys():
-    clause_map[clause] = [variable]
-  else:
-    clause_map[clause].append(variable)
-
-#Inputs: func_map is the map created in displaySolution that will store function atoms.
-#Purpose: Builds the function map by storing each function atom.
-def buildFuncMap(func_map, atom):
-  arguments = atom.split(')')[0].split('(')[1].split(',')
-  func = arguments[0]
-  id = arguments[1]
-  clause_number = arguments[2]
-  level = arguments[3]
-  clause = arguments[4]
-
-  if func not in func_map.keys():
-    func_map[func] = {}
-
-  clause_number_map = func_map[func]
-
-  if clause_number not in clause_number_map.keys():
-    clause_number_map[clause_number] = {}
-
-  id_map = clause_number_map[clause_number]
-  
-  if id not in id_map.keys():
-    id_map[id] = [(level,clause)]
-  else:
-    id_map[id].append((level,clause))
-
-#Inputs: term_map is the map containing term atoms, and func_map containing the function atoms (both created in displaySolution).
-#Purpose: Prints the contents of these maps.
-def printMaps(term_map, func_map):
-  if term_map:
-      print("Generated terms found \u2714\uFE0F  They are:")
-      for func in term_map.keys():
-        print("--Printing possible terms for function of " + func +"--")
-        level_map = term_map[func]
-        for level in level_map.keys():
-          print("Level " + level)
-          clause_map = level_map[level]
-          for clause in clause_map.keys():
-            if clause != list(clause_map.keys())[-1]:
-              print(clause + ":" + str(clause_map[clause]),  end=" \033[1;32m | \033[0;37;40m")
-            else:
-              print(clause + ":" + str(clause_map[clause]))
-
-      if func_map:
-        print("Function candidates found \u2B55  They are:")
-        for func in func_map.keys():
-          print("--Printing candidates for function of " + func +"--")
-          clause_number_map = func_map[func]
-          for clause_number in clause_number_map.keys():
-            print("Clause number: " + clause_number)
-            id_map = clause_number_map[clause_number]
-            for id in id_map.keys():
-              print(id + ":",  end=" ")
-              levels_clauses = id_map[id]
-              for lc in levels_clauses:
-                current_level = lc[0]
-                current_clause = lc[1]
-                func_clause = term_map[func][current_level][current_clause]
-                if lc != levels_clauses[-1]:
-                  print(str(func_clause), end=" or ")
-                else:
-                  print(str(func_clause))
-      else:
-        print("--No function candidates found \u274C --")
-
-#Inputs: atoms is the list of atoms given to printRepairs.
-#Purpose: Organizes term and function atoms into maps and prints them to output (other atoms are simply printed with as they are).
-def displaySolution(atoms):
-  repairs = []
+def termsMap(atoms):
   term_map = {}
-  func_map = {}
 
-  for a in atoms:
-    if "generated_term" in a:
-      buildTermMap(term_map,a)
-    elif "function_candidate" in a:
-      buildFuncMap(func_map,a)
-    else:
-      repairs.append(a)
+  for answer_set in atoms:
+    term = []
 
-  printMaps(term_map, func_map)
+    for a in answer_set:
+      if "generated_term" in a:
+        arguments = a.split(')')[0].split('(')[1].split(',')
+        func = arguments[0]
+        variable_number = arguments[1]
+        variable = arguments[2]
 
-  if repairs:
-    print("Other atoms found \U0001f440  They are:")
-    print(str(repairs))
+        term.append(variable)
+        
+        if func not in term_map.keys():
+          term_map[func] = {}
+
+        varnumber_map = term_map[func]
+        
+        if variable_number not in varnumber_map.keys():
+          varnumber_map[variable_number] = []
+
+        if(a == answer_set[-1] and term):
+          term_list = varnumber_map[variable_number]
+          term_list.append(term)
+
+  return term_map
+
+def printTermsMap(term_map):
+  print("Generated terms found \u2714\uFE0F  They are:")
+  for func in term_map.keys():
+    print("--Printing possible terms for function of " + func +"--")
+    varnumber_map = term_map[func]
+    for variable_number in dict(sorted(varnumber_map.items())).keys():
+      print("Variables per term: " + variable_number)
+      term_list = varnumber_map[variable_number]
+      for term in term_list:
+        if term != term_list[-1]:
+          print(str(term),  end=" \033[1;32m | \033[0;37;40m")
+        else:
+          print(str(term))
+
+def displayTerms(atoms):
+  term_map = termsMap(atoms)
+  printTermsMap(term_map)
 
 #Inputs: atoms is a list with the atoms obtained from solving with clingo.
 #Purpose: Prints clingo's solution in a more readable manner.
-def printRepairs(atoms):
+def printTerms(atoms):
   if not atoms:
     print("No answers sets could be found	\u2755 there must be something wrong with the encoding...")
   elif atoms[0]:
-    displaySolution(atoms)
+    displayTerms(atoms)
   else: 
-    print("No repairs could be found \u274C")
+    print("No atoms could be found \u274C")
 
 
 #-----Auxiliary clingo Functions-----
@@ -173,23 +116,24 @@ class Context:
     return N(combin)
 
 #-----Main-----
-clingo_args = []
+clingo_args = ["0"]
 if(debug_toggled):
-  clingo_args = ["--output-debug=text"]
+  clingo_args.append("--output-debug=text")
   
 ctl = clingo.Control(arguments=clingo_args)
 
 ctl.load(model_path)
 ctl.load(obsv_path)
 ctl.load(incst_path)
-ctl.load(repairs_path)
+ctl.load(termgen_path)
+ctl.load(funcgen_path)
 
 ctl.ground([("base", [])], context=Context)
-atoms = []
+terms = []
 with ctl.solve(yield_=True) as handle:
   for model in handle:
-    atoms = (str(model).split(" "))
+    terms.append(str(model).split(" "))
 
-#print(atoms)
-printRepairs(atoms)
+print(terms)
+printTerms(terms)
 printStatistics(ctl.statistics)
