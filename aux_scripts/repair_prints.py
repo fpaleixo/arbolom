5aa70987903f526e213d6c150ef336f134e4f1ce
Fred Aleixo <91429411+fpaleixo@users.noreply.github.com>
2022-06-30 21:23:31 +0100

    Improved encoding and solution display
    - Syntactic changes to encoding
    - Limited the node generator further

--- aux_scripts/repair_prints.py
+++ aux_scripts/repair_prints.py
@@ -21,33 +21,39 @@ def printRepairedLP(inconsistent_func, result):
   inhibitors = ""
   nodes = {}
 
-  for atom in result:
-    arguments = atom.split(')')[0].split('(')[1].split(',')
+  if not result:
+    print("No possible repairs found...")
 
-    if "activator" in atom:
-      activators += f"edge({arguments[0]}, {inconsistent_func}, 0).\n"
+  else:
+
+    for atom in result:
+      arguments = atom.split(')')[0].split('(')[1].split(',')
+
+      if "activator" in atom:
+        activators += f"edge({arguments[0]}, {inconsistent_func}, 0).\n"
+      
+      elif "inhibitor" in atom:
+        inhibitors += f"edge({arguments[0]}, {inconsistent_func}, 1).\n"
+
+      elif "regulator" in atom:
+        node_ID = arguments[0]
+        regulator = arguments[1]
+
+        if node_ID in nodes:
+          nodes[node_ID].append(regulator)
+        else:
+          nodes[node_ID] = [regulator]
+
+    result = f"%Regulators of {inconsistent_func}\n" + activators + inhibitors +"\n"
+    result += f"%Regulatory function of {inconsistent_func}\nfunction({inconsistent_func}, {len(nodes.keys())}).\n"
+
+    for node_ID in nodes.keys():
+      regulators = nodes[node_ID]
+
+      for reg in regulators:
+        result += f"term({inconsistent_func}, {node_ID}, {reg}).\n"
     
-    elif "inhibitor" in atom:
-      inhibitors += f"edge({arguments[0]}, {inconsistent_func}, 1).\n"
+    print("\033[1;32mRepairs: \033[0;37;40m")
 
-    elif "regulator" in atom:
-      node_ID = arguments[0]
-      regulator = arguments[1]
-
-      if node_ID in nodes:
-        nodes[node_ID].append(regulator)
-      else:
-        nodes[node_ID] = [regulator]
-
-  result = f"%Regulators of {inconsistent_func}\n" + activators + inhibitors +"\n"
-  result += f"%Regulatory function of {inconsistent_func}\nfunction({inconsistent_func}, {len(nodes.keys())}).\n"
-
-  for node_ID in nodes.keys():
-    regulators = nodes[node_ID]
-
-    for reg in regulators:
-      result += f"term({inconsistent_func}, {node_ID}, {reg}).\n"
-  
-  print("\033[1;32mRepairs: \033[0;37;40m")
-  print(result)
+    print(result)
 
